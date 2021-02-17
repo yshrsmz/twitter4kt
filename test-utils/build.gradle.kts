0710888739ce77cf7acbb14f90f704f94727873c
Yasuhiro SHIMIZU <the.phantom.bane@gmail.com>
2021-02-18 00:45:40 +0900

    :fire: remove unnecessary dependencies

--- test-utils/build.gradle.kts
+++ test-utils/build.gradle.kts
@@ -8,15 +8,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(Dependencies.Kotlin.serialization)
                 implementation(Dependencies.Kotlin.coroutines)
-                implementation(Dependencies.Kotlin.datetime)
-                implementation(Dependencies.Ktor.core)
-                implementation(Dependencies.Ktor.json)
-                implementation(Dependencies.Ktor.serialization)
-                implementation(Dependencies.okio)
-
-                api(project(Dependencies.Twitter4kt.coreModel))
             }
         }
         val commonTest by getting {
@@ -24,12 +16,10 @@ kotlin {
                 implementation(kotlin(Dependencies.Kotlin.Test.common))
                 implementation(kotlin(Dependencies.Kotlin.Test.annotations))
 
-                implementation(Dependencies.Ktor.logging)
             }
         }
         val jvmMain by getting {
             dependencies {
-                implementation(Dependencies.Ktor.okhttp)
             }
         }
         val jvmTest by getting {
