bc619446afa09a545f043e1fb3e8b39d2fbcd2fb
Yasuhiro SHIMIZU <the.phantom.bane@gmail.com>
2021-02-18 00:39:18 +0900

    :arrow_up: update dependencies

--- buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,14 +1,14 @@
 object Versions {
-    const val kotlin = "1.4.21"
+    const val kotlin = "1.4.30"
 
     const val mavenPublish = "0.13.0"
     const val versions = "0.36.0"
-    const val gradle = "6.8-rc-1"
+    const val gradle = "6.8.2"
     const val spotless = "5.8.2"
     const val ktlint = "0.39.0"
     const val dokka = "1.4.20"
 
-    const val ktor = "1.4.3"
+    const val ktor = "1.5.1"
 }
 
 object Plugins {
